519895f8f38e65ba7077a04adc86aa031b9e7f5d
Bartosz Pietrzak <b3k@b3k.pl>
2018-10-12 19:45:25 +0200

    Change parameter :backoff to :backoff_initial to avoid problem with redix

--- lib/exq/support/opts.ex
+++ lib/exq/support/opts.ex
@@ -1,5 +1,4 @@
 defmodule Exq.Support.Opts do
-
   alias Exq.Support.Coercion
   alias Exq.Support.Config
 
@@ -8,13 +7,13 @@ defmodule Exq.Support.Opts do
   """
   def top_supervisor(name) do
     name = name || Config.get(:name)
-    "#{name}.Sup" |> String.to_atom
+    "#{name}.Sup" |> String.to_atom()
   end
 
   defp conform_opts(opts) do
     mode = opts[:mode] || Config.get(:mode)
     redis = redis_client_name(opts[:name])
-    opts = [{:redis, redis}|opts]
+    opts = [{:redis, redis} | opts]
 
     redis_opts = redis_opts(opts)
     connection_opts = connection_opts(opts)
@@ -24,7 +23,7 @@ defmodule Exq.Support.Opts do
 
   def redis_client_name(name) do
     name = name || Config.get(:name)
-    "#{name}.Redis.Client" |> String.to_atom
+    "#{name}.Redis.Client" |> String.to_atom()
   end
 
   def redis_opts(opts \\ []) do
@@ -44,6 +43,7 @@ defmodule Exq.Support.Opts do
   """
   def redis_worker_opts(opts) do
     {redis_opts, connection_opts, opts} = conform_opts(opts)
+
     case Config.get(:redis_worker) do
       {module, args} -> {module, args, opts}
       _ -> {Redix, [redis_opts, connection_opts], opts}
@@ -62,7 +62,12 @@ defmodule Exq.Support.Opts do
     timeout = opts[:redis_timeout] || Config.get(:redis_timeout)
     socket_opts = opts[:socket_opts] || Config.get(:socket_opts) || []
 
-    [backoff: reconnect_on_sleep, timeout: timeout, name: opts[:redis], socket_opts: socket_opts]
+    [
+      backoff_initial: reconnect_on_sleep,
+      timeout: timeout,
+      name: opts[:redis],
+      socket_opts: socket_opts
+    ]
   end
 
   defp server_opts(:default, opts) do
@@ -85,14 +90,27 @@ defmodule Exq.Support.Opts do
     concurrency = get_concurrency(queue_configs, per_queue_concurrency)
     default_middleware = Config.get(:middleware)
 
-    [scheduler_enable: scheduler_enable, namespace: namespace,
-     scheduler_poll_timeout: scheduler_poll_timeout,workers_sup: workers_sup,
-     poll_timeout: poll_timeout, enqueuer: enqueuer, metadata: metadata,
-     stats: stats, name: opts[:name], scheduler: scheduler, queues:
-     queues, redis: opts[:redis], concurrency: concurrency,
-     middleware: middleware, default_middleware: default_middleware,
-     mode: :default, shutdown_timeout: shutdown_timeout]
+    [
+      scheduler_enable: scheduler_enable,
+      namespace: namespace,
+      scheduler_poll_timeout: scheduler_poll_timeout,
+      workers_sup: workers_sup,
+      poll_timeout: poll_timeout,
+      enqueuer: enqueuer,
+      metadata: metadata,
+      stats: stats,
+      name: opts[:name],
+      scheduler: scheduler,
+      queues: queues,
+      redis: opts[:redis],
+      concurrency: concurrency,
+      middleware: middleware,
+      default_middleware: default_middleware,
+      mode: :default,
+      shutdown_timeout: shutdown_timeout
+    ]
   end
+
   defp server_opts(mode, opts) do
     namespace = opts[:namespace] || Config.get(:namespace)
     [name: opts[:name], namespace: namespace, redis: opts[:redis], mode: mode]
@@ -108,12 +126,11 @@ defmodule Exq.Support.Opts do
   end
 
   defp get_concurrency(queue_configs, per_queue_concurrency) do
-    Enum.map(queue_configs, fn (queue_config) ->
-        case queue_config do
-          {queue, concurrency} -> {queue, concurrency, 0}
-          queue -> {queue, per_queue_concurrency, 0}
-        end
+    Enum.map(queue_configs, fn queue_config ->
+      case queue_config do
+        {queue, concurrency} -> {queue, concurrency, 0}
+        queue -> {queue, per_queue_concurrency, 0}
+      end
     end)
   end
-
 end
